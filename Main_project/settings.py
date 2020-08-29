c91840c660c60c9353239ca81a4f7a8347bbf6a6
kristoffer <kristowv@uio.no>
2020-08-29 17:14:05 +0200

    Changed staticfiles

--- Main_project/settings.py
+++ Main_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,4 +124,4 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'pages/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
